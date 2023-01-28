7089ef203e1aae41d29efa237ffee59801919d36
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2023-01-28 19:07:14 +0100

    Added a 'default' weapon, WeaponNone

---
+++ Patterns/StrategyPattern/Version3/Weapons/WeaponNone.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ProgrammingPatterns.Patterns.StrategyPattern.Version3.Weapons
+{
+    /// <summary>
+    /// Implementation of simply having no weapon equipped.
+    /// </summary>
+    internal class WeaponNone : WeaponBase
+    {
+        public WeaponNone() : base(
+                name: "None",
+                description: "Unarmed.",
+                alias: "",
+                damage: 0,
+                weaponType: WeaponType.None)
+        {
+        }
+    }
+}
