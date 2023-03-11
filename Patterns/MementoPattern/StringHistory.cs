3ae48ca88cb555b62a798d3645d60453b84ea0dd
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2023-03-11 01:20:47 +0100

    feat: Implementation of the IHistory<T> interface as a history of string 'content' objects with an undo function

---
+++ Patterns/MementoPattern/StringHistory.cs
@@ -0,0 +1,27 @@
+﻿namespace ProgrammingPatterns.Patterns.MementoPattern
+{
+    public class StringHistory : IHistory<string>
+    {
+        private Stack<string> _history;
+
+        public StringHistory()
+        {
+            _history = new Stack<string>();
+        }
+
+        public string Pop()
+        {
+            return _history.Pop();
+        }
+
+        public void Push(string historyElement)
+        {
+            _history.Push(historyElement);
+        }
+
+        public string Peek()
+        {
+            return _history.Peek();
+        }
+    }
+}
