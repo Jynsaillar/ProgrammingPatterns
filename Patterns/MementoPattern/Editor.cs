5820f75a575ac7e11ed632bcac15958694ba6a9f
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2023-03-11 01:15:40 +0100

    feat: Editor class to showcase the memeto pattern in action

---
+++ Patterns/MementoPattern/Editor.cs
@@ -0,0 +1,27 @@
+﻿namespace ProgrammingPatterns.Patterns.MementoPattern {
+    public class Editor
+    {
+        private string? _content;
+        private IHistory<string>? _history;
+
+        public Editor(string? content, IHistory<string>? history)
+        {
+            _content = content;
+            _history = history;
+        }
+
+        public string? GetContent() {
+            return _content;
+        }
+
+        public void SetContent(string content) {
+            this._content = content;
+            this._history?.Push(content);
+        }
+
+        public void Undo()
+        {
+            this._history?.Pop();
+            this._content =  this._history?.Peek();        }
+    }
+}
