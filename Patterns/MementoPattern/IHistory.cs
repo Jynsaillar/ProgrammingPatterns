55cb20a25df57e2bda51b335330867e7c9aeaae4
Jynsaillar <7015218+Jynsaillar@users.noreply.github.com>
2023-03-11 01:17:27 +0100

    feat: History interface, exposes the common stack function push, pop, peek

---
+++ Patterns/MementoPattern/IHistory.cs
@@ -0,0 +1,9 @@
+﻿namespace ProgrammingPatterns.Patterns.MementoPattern
+{
+    public interface IHistory<T>
+    {
+        public void Push(T historyElement);
+        public T Pop();
+        public T Peek();
+    }
+}
